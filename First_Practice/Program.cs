21535eb7d6db81cce61bae4928407a120c9d40fd
Farhan laeeq <farhanali27718@gmail.com>
2025-05-07 22:37:19 +0500

    changes added

--- First_Practice/Program.cs
+++ First_Practice/Program.cs
@@ -11,6 +11,7 @@ namespace First_Practice
         static void Main(string[] args)
         {
             // Strings
+            Console.WriteLine("-------------STRINGS--------------");
             Console.WriteLine("Enter Your First Name: ");
             string fname = Console.ReadLine();
             Console.WriteLine("Enter Your Last Name: ");
@@ -18,9 +19,9 @@ namespace First_Practice
             Console.WriteLine("Your name is " + fname + " " + lname + "."); //String concatination
             Console.WriteLine("Your name is {0} {1}.", fname, lname); //Placeholder syntax (preferred)
 
-            Console.WriteLine("\n\n---------------------------\n\n");
 
             // Integers
+            Console.WriteLine("\n\n-------------INTEGERS--------------");
             Console.WriteLine("Enter first Number: ");
             int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter second Number: ");
@@ -28,6 +29,22 @@ namespace First_Practice
 
             int sum = num1 + num2;
             Console.WriteLine("Total sum is {0} + {1} = {2}", num1, num2, sum);
+
+
+            // Arithmetic Operators +, -, *, /, %
+            Console.WriteLine("\n\n--------------ARITHMETIC OPERATORS-------------");
+            int number1 = 0, number2 = 0, totalSum;
+            Console.WriteLine("Enter first Number: ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second Number: ");
+            number2 = int.Parse(Console.ReadLine());
+
+            totalSum = number1 + number2;
+            Console.WriteLine("Addition: {0} + {1} = {2}", number1, number2, totalSum);
+            Console.WriteLine("Subtraction: {0} - {1} = {2}", number1, number2, number1-number2);
+            Console.WriteLine("Multiplication: {0} * {1} = {2}", number1, number2, number1*number2);
+            Console.WriteLine("Division: {0} / {1} = {2}", number1, number2, number1/number2);
+            Console.WriteLine("Modulus: {0} % {1} = {2}", number1, number2, number1%number2);
         }
     }
 }
